d36865ff1fac6dc65cf5505ad96c9c40d6362797
Valentin <pamerra@mail.ru>
2025-08-26 16:54:49 +0300

    63. 4/13. Демонстрация использования Image?

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -8,26 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
-    
+    @State private var image: Image?
+     
     var body: some View {
         VStack {
-            Button("Hello, world!") {
-                showingConfirmation.toggle()
-            }
-            .frame(width: 300, height: 300)
-            .padding()
-            .background(backgroundColor)
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") {backgroundColor = .red }
-                Button("Green") {backgroundColor = .green }
-                Button("Blue") {backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+            image?
+                .resizable()
+                .scaledToFit()
         }
+        .onAppear(perform: loadImage)
+    }
+    private func loadImage() {
+        image = Image(.example)
     }
 }
 
