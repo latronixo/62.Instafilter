6819d20a6926cbd33a14843db0192b12575c711a
Valentin <pamerra@mail.ru>
2025-08-26 21:29:46 +0300

    63. 5/13. Использование ContentUnavailableView с кнопкой

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContentUnavailableView("No snippets", systemImage: "swift")
+        ContentUnavailableView{
+            Label("No snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet")
+        } actions: {
+            Button("Create snippet") {
+                //create a snippet
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
