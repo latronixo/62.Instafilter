90b416cc38d9d46936dd10f1e799891c62b5771f
Valentin <pamerra@mail.ru>
2025-08-26 21:24:19 +0300

    63. 5/13. Использование ContentUnavailableView для случая когда контента для отображения еще нет. Короткий вариант

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContentUnavailableView("No snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet"))
+        ContentUnavailableView("No snippets", systemImage: "swift")
     }
 }
 
