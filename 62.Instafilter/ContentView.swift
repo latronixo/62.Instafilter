07c89d665b7001aae2f9a5eabafbf1e98d99a23d
Valentin <pamerra@mail.ru>
2025-08-26 17:35:00 +0300

    63. 4/13. CoreImage для применения фильтра sepia

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Валентин on 26.08.2025.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
@@ -18,8 +20,22 @@ struct ContentView: View {
         }
         .onAppear(perform: loadImage)
     }
+    
     private func loadImage() {
-        image = Image(.example)
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
