0e2648d0d2e15e0ffc2cbcab70a57599b687eff3
Valentin <pamerra@mail.ru>
2025-08-26 16:19:48 +0300

    62. 3/13. Диалоговое окно с несколькими вариантами выбора пользователя .confirmationDialog

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            Slider(value: $blurAmount, in: 0...20)
+            Button("Hello, world!") {
+                showingConfirmation.toggle()
+            }
+            .frame(width: 300, height: 300)
+            .padding()
+            .background(backgroundColor)
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") {backgroundColor = .red }
+                Button("Green") {backgroundColor = .green }
+                Button("Blue") {backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
         }
-        .onChange(of: blurAmount) { 
-            print("Blur changed")
-        }
-        .padding()
     }
 }
 
