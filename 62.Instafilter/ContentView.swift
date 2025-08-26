70607e3a5c854e706172b70a9e4bc8c1bd9188da
Valentin <pamerra@mail.ru>
2025-08-26 14:48:38 +0300

    Initial Commit

---
+++ 62.Instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  62.Instafilter
+//
+//  Created by Валентин on 26.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 62.Instafilter/_2_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_InstafilterApp.swift
+//  62.Instafilter
+//
+//  Created by Валентин on 26.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct _2_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
