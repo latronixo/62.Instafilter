40992e80a1d52176767f44b042e8815e6eb3aa84
Valentin <pamerra@mail.ru>
2025-08-27 16:25:21 +0300

    66. 13/13.  Добавил кнопку Поделиться измененным изображением

--- 62.Pr13.Instafilter/ContentView.swift
+++ 62.Pr13.Instafilter/ContentView.swift
@@ -51,7 +51,9 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    // share the picture
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
                 }
             }
             .padding([.horizontal, .bottom])
