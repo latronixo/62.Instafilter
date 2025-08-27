a9e00dd5c3d646f38eaa1a28eb2a392229a17b51
Valentin <pamerra@mail.ru>
2025-08-27 11:50:25 +0300

    64. 6/13. Можно ограничить выбор по типу, но без скриншотов

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .images) {
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
                 Label("Select a picture", systemImage: "photo")
             }
             
