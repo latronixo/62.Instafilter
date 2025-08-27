8f71ea11c6c4033c13698c5f78dbe9d3210b05bf
Valentin <pamerra@mail.ru>
2025-08-27 13:49:18 +0300

    64. 7/13. Демонстрация создания элемента ShareLink - Поделиться

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -9,33 +9,8 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
-    
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select a picture", systemImage: "photo")
-            }
-            
-            ScrollView {
-                ForEach(0..<selectedImages.count, id: \.self) { i in
-                    selectedImages[i]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        }
-        .onChange(of: pickerItems) {
-            Task {
-                selectedImages = []
-                for pickerItem in pickerItems {
-                    if let loadedImage = try await pickerItem.loadTransferable(type: Image.self) {
-                        selectedImages.append(loadedImage)
-                    }
-                }
-            }
-        }
+        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!)
     }
 }
 
