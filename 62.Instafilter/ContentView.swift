93835d4094c8542f47a724e7d67deeb0c7d0474f
Valentin <pamerra@mail.ru>
2025-08-27 14:30:09 +0300

    64. 7/13. Поделиться изображением, с превью

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -5,13 +5,13 @@
 //  Created by Валентин on 26.08.2025.
 //
 
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
-            Label("Srpread the word about Swift", systemImage: "swift")
+        let example = Image(.example)
+        ShareLink(item: example, preview: SharePreview("картинка из приложения BestRecipe", image: example)) {
+            Label("click to share", systemImage: "airplane")
         }
     }
 }
