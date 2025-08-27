e642d4f4be445228d404f3cd6942f0cbd3796566
Valentin <pamerra@mail.ru>
2025-08-27 13:49:39 +0300

    64. 7/13. Расширенная версия ShareLink

--- 62.Instafilter/ContentView.swift
+++ 62.Instafilter/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!)
+        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!, subject: Text("Learn Swift here"), message: Text("Check out the 100 Days of SwiftUI"))
     }
 }
 
