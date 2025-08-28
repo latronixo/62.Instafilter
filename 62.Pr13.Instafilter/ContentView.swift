dfca11833a7268ca15cc86573378741b4a9bf036
Valentin <pamerra@mail.ru>
2025-08-28 20:57:28 +0300

    67.Ch2. Сделал три слайдера вместо одного: для интенсивности, радиуса и scale

--- 62.Pr13.Instafilter/ContentView.swift
+++ 62.Pr13.Instafilter/ContentView.swift
@@ -14,6 +14,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
     @State private var selectedItem: PhotosPickerItem?
     @State private var showingFilters = false
     @State private var imageDidLoaded = false
@@ -48,7 +50,21 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
+                        .onChange(of: filterIntensity, applyIntensity)
+                        .disabled(!imageDidLoaded)
+                }
+                
+                HStack {
+                    Text("Radius")
+                    Slider(value: $filterRadius)
+                        .onChange(of: filterRadius, applyRadius)
+                        .disabled(!imageDidLoaded)
+                }
+                
+                HStack {
+                    Text("Scale")
+                    Slider(value: $filterScale)
+                        .onChange(of: filterScale, applyScale)
                         .disabled(!imageDidLoaded)
                 }
                 
@@ -93,20 +109,38 @@ struct ContentView: View {
             
             let beginImage = CIImage(image: inputImage)
             currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-            applyProcessing()
+            applyIntensity()
         }
     }
     
-    func applyProcessing () {
+    func applyIntensity () {
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey) {
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        
+        applyfilters()
+    }
+    
+    func applyRadius () {
+        let inputKeys = currentFilter.inputKeys
+        
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        
+        applyfilters()
+    }
+    
+    func applyScale () {
+        let inputKeys = currentFilter.inputKeys
+        
         if inputKeys.contains(kCIInputScaleKey) {
             currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
         
+        applyfilters()
+    }
+    
+    private func applyfilters() {
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
         
